c5e50302663f0d4cc0f59689a64f7f12163b89b5
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2022-01-12 22:10:50 +0300

    Homework is done for the second lesson

--- Introduction in Swift (Lesson 2)/Introduction in Swift (Lesson 2)/tools.swift
+++ Introduction in Swift (Lesson 2)/Introduction in Swift (Lesson 2)/tools.swift
@@ -58,3 +58,43 @@ func getDoubleFromUserIf(message: String, condition: (Double) -> Bool) -> Double
 func getDoubleFromUser(message: String) -> Double {
     getDoubleFromUserIf(message: message, condition: ({_ in true}))
 }
+
+func homeworkPresentation(message: String, homework: Array<(description: String, task: () -> Void)>) -> Void {
+    let stopWord: String = "exit"
+    let postfix: String = "Type '\(stopWord)' for exit."
+    
+    var isContinue: Bool = true
+    var fullMessage: String = "\n" + message + "\n"
+    
+    for work in homework {
+        fullMessage += "\t" + work.description + "\n"
+    }
+    
+    fullMessage += postfix + "\n"
+    
+    repeat {
+        print(fullMessage)
+
+        let rawInput: String? = readLine()
+
+        if let input = rawInput {
+            if input == stopWord {
+                isContinue = false
+            } else {
+                if let index = Int(input) {
+                    if index >= 1 && index <= homework.count {
+                        let task = homework[index - 1].task
+                        task()
+                    } else {
+                        print("Wrong task index!")
+                    }
+                } else {
+                    print("Unsupported input")
+                }
+            }
+        } else {
+            print("Unsupported input")
+        }
+
+    } while isContinue
+}
